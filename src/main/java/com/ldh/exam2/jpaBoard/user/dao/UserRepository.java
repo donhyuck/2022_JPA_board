a8ea590a44cfd176b931d6498769143bb08ec2a6
donhyuck <leedonhyuck123@gmail.com>
2022-03-26 23:10:23 +0900

    DB에서 유저 정보 가져오기

---
+++ src/main/java/com/ldh/exam2/jpaBoard/user/dao/UserRepository.java
@@ -0,0 +1,7 @@
+package com.ldh.exam2.jpaBoard.user.dao;
+
+import com.ldh.exam2.jpaBoard.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}

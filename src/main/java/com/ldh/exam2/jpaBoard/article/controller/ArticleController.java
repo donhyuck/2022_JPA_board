d77ba1184b0917001936bbb929e502cc2536d75e
donhyuck <leedonhyuck123@gmail.com>
2022-03-27 21:41:56 +0900

    DB의 게시글 정보 출력 / 게시글 전체목록 보기 기능 추가

--- src/main/java/com/ldh/exam2/jpaBoard/article/controller/ArticleController.java
+++ src/main/java/com/ldh/exam2/jpaBoard/article/controller/ArticleController.java
@@ -1,15 +1,25 @@
 package com.ldh.exam2.jpaBoard.article.controller;
 
 import com.ldh.exam2.jpaBoard.article.dao.ArticleRepository;
+import com.ldh.exam2.jpaBoard.article.domain.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("user/article")
+@RequestMapping("/user/article")
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
 
+    // 게시글 목록 보기
+    @RequestMapping("showList")
+    @ResponseBody
+    private List<Article> showList() {
+        return articleRepository.findAll();
+    }
 
 }

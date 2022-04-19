f69f79b904bf8070ab93e2f8386d9bad53d1a37f
donhyuck <leedonhyuck123@gmail.com>
2022-04-19 12:36:45 +0900

    체크

--- src/main/java/com/ldh/exam2/jpaBoard/article/controller/ArticleController.java
+++ src/main/java/com/ldh/exam2/jpaBoard/article/controller/ArticleController.java
@@ -5,6 +5,7 @@ import com.ldh.exam2.jpaBoard.article.domain.Article;
 import com.ldh.exam2.jpaBoard.user.dao.UserRepository;
 import com.ldh.exam2.jpaBoard.user.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;

9c83fe88f5d44e6e374d3eaca7011368299dd4e5
donhyuck <leedonhyuck123@gmail.com>
2022-04-20 17:43:00 +0900

    게시글 기능에서 공백 처리

--- src/main/java/com/ldh/exam2/jpaBoard/article/controller/ArticleController.java
+++ src/main/java/com/ldh/exam2/jpaBoard/article/controller/ArticleController.java
@@ -62,24 +62,7 @@ public class ArticleController {
         }
 
         // 공백(미입력) 확인
-        if (title == null || title.trim().length() == 0) {
-            return """
-                    <script>
-                    alert('제목을 입력해주세요.');
-                    history.back();
-                    </script>
-                    """;
-        }
         title = title.trim();
-
-        if (body == null || body.trim().length() == 0) {
-            return """
-                    <script>
-                    alert('내용을 입력해주세요.');
-                    history.back();
-                    </script>
-                    """;
-        }
         body = body.trim();
 
         // 게시글 등록
